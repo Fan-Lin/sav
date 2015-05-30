1e4671966450a10eae138b319ea316048d821690
Fan-Lin <1193968991@qq.com>
2015-05-30 15:27:21 +0800

    new

---
+++ sav/sav/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float r;
+            float d;
+            float s;
+            float Result;
+            int Type;
+            int Type2;
+            Console.WriteLine("你想要计算什么？\n体积请输入1 表面积请输入2\n----------------------------------------");
+            Type = Convert.ToInt16(Console.ReadLine());
+            switch (Type)
+            {
+                case 1:
+                    break;
+                case 2:
+                    braek;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
